0060573fbc745135268130a2d2b68b546d5e617f
jorolato <jorolato@gmail.com>
2023-08-03 14:30:43 +0800

    slight  progress to text input

--- slag.py
+++ slag.py
@@ -15,7 +15,6 @@ def two_way_automaton(input_string):
     current_state = 'q0'
     index = 0
     stack = ['Z']
-
     for trans in transition:
         current_state = trans[0]
         read = trans[1]
@@ -23,86 +22,48 @@ def two_way_automaton(input_string):
         direction = trans[3]
         nxtstate = trans[4]
         push = trans[5]
-        """
-        if current_state == 'q0':
+
+        if current_state == nxtstate:
+            next = nxtstate
+        else:
+            next = current_state
+
+        if next == current_state :
             if index < len(input_string):
                 symbol = input_string[index]
-                if symbol == '0':
-                    index += 1
-                    stack.append('X')
-                    print(current_state, index, stack)
-                elif symbol == '1':
-                    current_state = 'q1'
+                if symbol == read:
+                    if pop != '@':
+                        stack.pop()
+                    if direction == '1':
+                        index += 1
+                    elif direction == '0':
+                        index = index
+                    elif direction == '-1':
+                        index -= 1
+
+
+                    if push != '@':
+                        stack.append(push)
+            print(current_state, index, stack)
+
+    """
+    print('next:',next,'\n','current state:', current_state)
+            if current_state == 'q0':
+                if index < len(input_string):
+                    symbol = input_string[index]
+                    if symbol == '0':
+                        index += 1
+                        stack.append('X')
+                        print(current_state, index, stack)
+                    elif symbol == '1':
+                        current_state = 'q1'
+                    else:
+                        return False
                 else:
-                    return False
-            else:
-                break
-        """
+                    break
+            """
 
-        if index < len(input_string):
-            symbol = input_string[index]
-            if symbol == read and symbol == '0':
 
-                if current_state == 'q1' and read == '2':
-                    index = len(input_string)-1
-
-                if direction == '1':
-                    index += 1
-                elif direction == '-1':
-                    index -= 1
-                else:
-                    index = index
-
-                if push != '@':
-                    stack.append(push)
-
-                if pop != '@':
-                    stack.pop()
-
-                next_state = nxtstate
-                print(current_state,index,stack)
-
-            elif symbol == read and symbol == '1':
-
-                if current_state == 'q1' and read == '2':
-                    index = len(input_string) - 1
-
-                if direction == '1':
-                    index += 1
-                elif direction == '-1':
-                    index -= 1
-                else:
-                    index = index
-
-                if push != '@':
-                    stack.append(push)
-
-                if pop != '@':
-                    stack.pop()
-
-                next_state = nxtstate
-                print(current_state, index, stack)
-
-            if symbol == read and symbol == '2':
-
-                if current_state == 'q1' and read == '2':
-                    index = len(input_string)-1
-
-                if direction == '1':
-                    index += 1
-                elif direction == '-1':
-                    index -= 1
-                else:
-                    index = index
-
-                if push != '@':
-                    stack.append(push)
-
-                if pop != '@':
-                    stack.pop()
-
-                next_state = nxtstate
-                print(current_state,index,stack)
 
 
 
