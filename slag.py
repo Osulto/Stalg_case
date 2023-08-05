f717afe1b32ea415bbf00a2d30321ffac0b08c38
jorolato <jorolato@gmail.com>
2023-08-05 23:42:29 +0800

    DONE (FOR DOUBLE CHECKING)

--- slag.py
+++ slag.py
@@ -14,102 +14,59 @@ def two_way_automaton(input_string):
 
     for trans in transition:
 
-        read = trans[1]
         current_state = trans[0]
+        read = trans[1]
         pop = trans[2]
         direction = trans[3]
         nxtstate = trans[4]
         push = trans[5]
 
-        if current_state == nxtstate:
-            next = nxtstate
-        else:
-            next = current_state
-
         symbol = input_string[index]
-        """
-        if index < len(input_string) :
-            print('before', symbol)
-            if index != len(input_string)-1:
-                index += 1
-            print('after:', symbol)
-        """
+
         while symbol == read and index < len(input_string):
             symbol = input_string[index]
-            print('state', current_state, 'index:', index, 'symbol', symbol, 'read', read)
-            print(stack)
-            print('============')
-            if index < len(input_string):
+            if index < len(input_string) and symbol == read:
 
                 if direction == 'R':
                     index += 1
                 elif direction == 'L':
-                    print('LEFT')
                     index -= 1
                 elif direction == '0':
-                    print('STAY')
                     index = index
 
                 if pop != '@':
-                    print('POP')
-                    stack.pop()
+                    if len(stack) >= 1:
+                        stack.pop()
 
                 if push != '@' and symbol == read:
                     stack.append(push)
-
             else:
-                print('break')
                 break
 
+            print('Current State:', current_state)
+            print('String Char:', symbol)
+            print('Direction:', direction)
+            print('Stack:', stack)
+            print('================================')
 
+    if len(stack) == 1:
+        return True
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    """""""""
-    if next == current_state:
-        while index < len(input_string):
-            symbol = input_string[index]
-            print('symbol:', symbol)
-            print('read', read)
-            print('index', index)
-            print('===========================')
-            if symbol != read:
-                break
-            else:
-                if pop != '@':
-                    print('POP')
-                    stack.pop()
-
-                if direction == '1':
-                    index += 1
-
-                elif direction == '0':
-                    index = index
-
-                elif direction == '-1':
-                    index -= 1
-
-                if push != '@':
-                    stack.append(push)
-
-        print('state:', current_state, stack)
-"""""""""
 
 # Main code to test the automaton
 if __name__ == "__main__":
-    input_string = input("Enter the input string (consisting of '0's, '1's): ")
+    input_string = input("Enter a string consisting of 0's and 1's \n put a '>' by the end of it: ")
     if two_way_automaton(input_string):
-        print("Accepted.")
+        print("[ACCEPTED STRING]")
     else:
-        print("Not Accepted.")
+        print("[INVALID STRING]")
+
+"""""
+Text File Format:
+States: q0,q1,q2,q3
+Inputs: 0,1
+Initial State: q0
+Final State: q3
+Transitions q0,0,@,R,q0,X:
+Current State | Read | Pop | Direction | Next State | Push
+"""""
